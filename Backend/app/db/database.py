886c14a8241f6bba73775a2226929f408a408cef
Aastha <262373115+acorn266@users.noreply.github.com>
2026-09-25 12:57:57 +0530

    Fix PostgreSQL database connection

--- Backend/app/db/database.py
+++ Backend/app/db/database.py
@@ -4,11 +4,17 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from app.core.config import settings
 
 
-engine = create_engine(
-    settings.DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    pool_pre_ping=True,
-)
+if settings.DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        settings.DATABASE_URL,
+        connect_args={"check_same_thread": False},
+        pool_pre_ping=True,
+    )
+else:
+    engine = create_engine(
+        settings.DATABASE_URL,
+        pool_pre_ping=True,
+    )
 
 
 SessionLocal = sessionmaker(
@@ -23,7 +29,6 @@ Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
-
     try:
         yield db
     finally:
